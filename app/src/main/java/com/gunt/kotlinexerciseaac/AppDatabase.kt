981550bda84440d86cb4cc7cc1205f348c6fe49b
geon <0408gun@gmail.com>
2020-12-16 15:06:02 +0900

    [feat] Room 적용(kotlin)
    editText의 내용 Room을 이용해 저장하고 화면 출력 기능 추가

---
+++ app/src/main/java/com/gunt/kotlinexerciseaac/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.gunt.kotlinexerciseaac
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDAO(): TodoDAO
+}
